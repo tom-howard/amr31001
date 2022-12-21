3a7690a9c0c240f68636d0f892746c6d11900964
Tom Howard <t.howard@sheffield.ac.uk>
2022-12-21 10:31:04 +0000

    changed to a very basic left-hand wall follower

--- src/ex2.py
+++ src/ex2.py
@@ -13,12 +13,26 @@ motion = waffle.Motion()
 pose = waffle.Pose()
 lidar = waffle.Lidar()
 
+lin_vel = 0.0
+ang_vel = 0.0
+
 while not rospy.is_shutdown():
-    lidar.distance.readings()
-    if (lidar.distance.front < 0.4) or (lidar.distance.r1 < 0.3):
-        motion.move_at_velocity(linear=0.0, angular=0.2)
+
+    lin_vel = 0.05
+
+    diff = lidar.distance.l3 - lidar.distance.l4
+    print(diff)
+
+    if abs(diff) < 0.001:
+        print("straight")
+        ang_vel = 0.0
+    elif diff < 0:
+        print("turn right")
+        ang_vel = -0.2
+        motion.move_at_velocity(angular=-0.2)
     else:
-        motion.move_at_velocity(linear=0.1, angular=0.0)
-        
+        print("turn left")
+        ang_vel = 0.2
+    motion.move_at_velocity(linear=lin_vel, angular=ang_vel)
     rate.sleep()
     
