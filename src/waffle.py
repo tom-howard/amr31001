95c4c1901bc4f2d1c719cc7f1e082f970196d5f1
Tom Howard <t.howard@sheffield.ac.uk>
2023-01-12 16:43:43 +0000

    better velocity limiting

--- src/waffle.py
+++ src/waffle.py
@@ -15,14 +15,19 @@ class Motion():
         self.vel_cmd = Twist()
 
     def move_at_velocity(self, linear = 0.0, angular = 0.0):
-        if abs(linear) > 0.26 or abs(angular) > 1.82:
-            print(f"Requested velocity cannot be achieved!")
-            print(f" * LINEAR velocity request: {linear} (m/s) [limit = +/-0.26 (m/s)].")
-            print(f" * ANGULAR velocity request: {angular} (rad/s) [limit = +/-1.82 (rad/s)].")
-        else:
-            self.vel_cmd.linear.x = linear
-            self.vel_cmd.angular.z = angular
-            self.publish()
+        if abs(linear) > 0.26:
+            print("Requested LINEAR velocity cannot be achieved!")
+            print(f" * Velocity limit = +/-0.26 (m/s).")
+            linear = np.sign(linear) * 0.26
+        
+        if abs(angular) > 1.82:
+            print(f"Requested ANGULAR velocity cannot be achieved!")
+            print(f" * Velocity limit = +/-1.82 (rad/s).")
+            angular = np.sign(angular) * 1.82
+        
+        self.vel_cmd.linear.x = linear
+        self.vel_cmd.angular.z = angular
+        self.publish()
         
     def stop(self):
         self.move_at_velocity()
