708b437801b1a349d2ed109341d6429ee97bb6f2
Tom Howard <t.howard@sheffield.ac.uk>
2023-01-23 13:30:45 +0000

    remove build.sh

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-!/usr/bin/env bash
-
-rm -rf ~/catkin_ws/src/amr31001/
-cd ~/catkin_ws/src/
-git clone --branch main --single-branch --depth=1 https://github.com/tom-howard/amr31001.git
-catkin build amr31001
-
